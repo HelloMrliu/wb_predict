92506732008bd7a2d8d5e0730399fa925dffaf01
lyp <806167947@qq.com>
2016-09-18 18:58:18 +0800

    lyp

---
+++ test_predict/make_depth_classify.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from __future__ import unicode_literals
+import codecs
+import os
+
+
+weibo_depth_dir = os.path.join(os.pardir, 'middle_data', 'weibo_time_depth.txt')
+weibo_testcase_depth_dir = os.path.join(os.pardir, 'badcase_testcase_data', 'weibo_testcase_depth.txt')
+weibo_badcase_depth_dir = os.path.join(os.pardir, 'badcase_testcase_data', 'weibo_badcase_depth.txt')
+weibo_classify_save_file_path = os.path.join(os.pardir, 'middle_data', 'weibo_classify_data.txt')
---
+++ test_predict/make_width_classify.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from __future__ import unicode_literals
+import codecs
+import os
+
+
+weibo_width_dir = os.path.join(os.pardir, 'middle_data', 'weibo_time_width.txt')
+weibo_testcase_width_dir = os.path.join(os.pardir, 'badcase_testcase_data', 'weibo_testcase_width.txt')
+weibo_badcase_width_dir = os.path.join(os.pardir, 'badcase_testcase_data', 'weibo_badcase_width.txt')
+save_file_path = os.path.join(os.pardir, 'middle_data', 'test_data_same_list.txt')
+weibo_classify_save_file_path = os.path.join(os.pardir, 'middle_data', 'weibo_classify_data.txt')
